9645b47dd3af6642cd88cb8a9f29fb429e318a7a
Marco Araya <maraya@icci.cl>
2017-06-15 01:53:35 -0400

    Validaciones Usuario

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,8 @@ class User < ActiveRecord::Base
   validates :Apellido,presence: true
   validates :Rut, rut: true , uniqueness: {case_sensitive: false ,message: "ya esta registrado"}
   validates :Direccion,presence: true
-  validates :Telefono,presence: true
-  validates :Fonoalt,presence: true
+  validates :Telefono,format:{ :with =>/\A[9]{1}[0-9]{8}\z/ , :message =>"Invalido"}
+  validates :Fonoalt,format:{ :with =>/\A[2|9]{1}[0-9]{8}\z/ , :message =>"Invalido"}
 
 
   devise :database_authenticatable, :registerable,
